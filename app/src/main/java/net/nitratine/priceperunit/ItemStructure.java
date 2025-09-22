a15623a7f290771746eafb3edae833d99f5ac8b2
miguelsp <miansupa@gmail.com>
2025-09-22 11:27:09 +0200

    Cambio a com.pijama.ComparaPrecios

--- app/src/main/java/net/nitratine/priceperunit/ItemStructure.java
+++
@@ -1,9 +0,0 @@
-package net.nitratine.priceperunit;
-
-public class ItemStructure {
-    public String name;
-    public String price;
-    public String quantity;
-    public String size;
-    public String unit;
-}
